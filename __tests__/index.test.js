8dfd7f18005ece4718f035ae249344722e5753f7
mettled <smettled@gmail.com>
2020-01-13 22:50:09 +0300

    refactor code

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,18 +3,8 @@ import gendiff from '../src';
 
 const path1 = `${__dirname}/../__fixture__/before.json`;
 const path2 = `${__dirname}/../__fixture__/after.json`;
-const result = fs.readFileSync(`${__dirname}/../__fixture__/result.js`, 'utf8', 'r');
-
-const rez = 
-`{
-   host: hexlet.io
- + timeout: 20
- - timeout: 45
- - proxy: 123.234.53.22
- - follow: false
- + verbose: true
-}`;
+const result = fs.readFileSync(`${__dirname}/../__fixture__/result.txt`, 'utf8', 'r').trim();
 
 test('test1', () => {
-  expect(gendiff(path1, path2)).toBe(rez);
+  expect(gendiff(path1, path2)).toBe(result);
 });
