8243af54a0f4d748822d0f47f21d4d883591cf74
mettled <smettled@gmail.com>
2020-01-18 00:33:55 +0300

    add tree implementation

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,11 +3,12 @@ import gendiff from '../src';
 
 test.each([
   ['json', 'before', 'after', 'resultJson.txt'],
+  ['json', 'before2', 'after2', 'resultJson2.txt'],
   ['yaml', 'before', 'after', 'resultYaml.txt'],
   ['ini', 'before', 'after', 'resultIni.txt'],
-])('test different two %s files', (format, before, after, resultFile) => {
-  const path1 = `${__dirname}/../__fixture__/before.${format}`;
-  const path2 = `${__dirname}/../__fixture__/after.${format}`;
+])('test different two %s files : %s and %s', (format, before, after, resultFile) => {
+  const path1 = `${__dirname}/../__fixture__/${before}.${format}`;
+  const path2 = `${__dirname}/../__fixture__/${after}.${format}`;
   const result = fs.readFileSync(`${__dirname}/../__fixture__/${resultFile}`, 'utf8', 'r').trim();
 
 
--- src/index.js
+++ src/index.js
@@ -1,80 +1,46 @@
 import parseContentFromFile from './parser';
+import render from './render';
 
 const properties = [
   {
-    state: 'object',
     check: (value1, value2) => value1 instanceof Object && value2 instanceof Object,
-    property: (name, value1, value2) => ({ state: 'object', name, value1: getDiff(value1, value2) }),
+    property: (f, name, value1, value2) => ({ state: 'object', name, value1: f(value1, value2) }),
   },
   {
-    state: 'remote',
     check: (value1, value2) => typeof value1 !== 'undefined' && typeof value2 === 'undefined',
-    property: (name, value1) => ({ name, value1 }),
+    property: (f, name, value1) => ({ state: 'remote', name, value1: f(value1, value1) }),
   },
   {
-    state: 'add',
     check: (value1, value2) => typeof value1 === 'undefined' && typeof value2 !== 'undefined',
-    property: (name, value1, value2) => ({ name, value1: value2 }),
+    property: (f, name, value1, value2) => ({ state: 'add', name, value1: f(value2, value2) }),
   },
   {
-    state: 'noEqual',
     check: (value1, value2) => value1 !== value2,
-    property: (name, value1, value2) => ({ name, value1, value2 }),
+    property: (f, name, value1, value2) => ({
+      state: 'noEqual', name, value1: f(value1, value1), value2: f(value2, value2),
+    }),
   },
   {
-    state: 'equal',
     check: (value1, value2) => value1 === value2,
-    property: (name, value1) => ({ name, value1 }),
+    property: (f, name, value1) => ({ state: 'equal', name, value1: f(value1, value1) }),
   },
 ];
 
-const checkValueByChildren = (value, actionFunction) => {
-  console.log('check', (value instanceof Object), value)
-  return (value instanceof Object) ? actionFunction(value) : value
-};
+const getDiff = (dataFirst = {}, dataSecond = {}) => {
+  if (!(dataFirst instanceof Object)) {
+    return dataFirst;
+  }
 
+  if (!(dataSecond instanceof Object)) {
+    return dataSecond;
+  }
 
-const render = (data) => {
-  const resultString = [...data].map((item) => {
-    const { state, property: { name, value1, value2 } } = item;
-       console.log('1 --- \n', state, name, value1, value2, item )
-    let res ;
-    if (state === 'object') {
-      res = `${name}: {${checkValueByChildren(value1, render)}\n}` 
-    }
-    if (state === 'remote') {
-      res = `- ${name}: ${checkValueByChildren(value1, render)}`;
-
-    }
-
-   if (state === 'add') {
-      res =`+ ${name}: ${checkValueByChildren(value1, render)}`;
-    }
-
-   if (state === 'noEqual') {
-       res =`- ${name}: ${checkValueByChildren(value1, render)}\n+ ${name}: ${checkValueByChildren(value2, render)}
-     `;
-    }
-
-   if (state === 'equal') {
-      res =` ${name}: ${checkValueByChildren(value1, render)}`;
-    }
-    console.log('res ----\n ',`\n${res}\n`);
-return res;
-
-
-  }).join('\n');
-  console.log('resStr - \n ', resultString);
-  return resultString;
-}
-
-const getDiff = (dataFirst, dataSecond) => {
   const unionKeys = new Set([...Object.keys(dataFirst), ...Object.keys(dataSecond)]);
   const resultCompare = [...unionKeys].map((key) => {
     const value1 = dataFirst[key];
     const value2 = dataSecond[key];
-    const { state, property } = properties.find(({ check }) => check(value1, value2));
-    return { state, property: property(key, value1, value2) };
+    const { property } = properties.find(({ check }) => check(value1, value2));
+    return property(getDiff, key, value1, value2);
   });
   return resultCompare;
 };
@@ -83,6 +49,5 @@ export default (path1, path2) => {
   const dataFirst = parseContentFromFile(path1);
   const dataSecond = parseContentFromFile(path2);
   const a = getDiff(dataFirst, dataSecond);
-  const b = render([...a]);
-  return b;
+  return `{\n${render(a, 0)}\n}`;
 };
