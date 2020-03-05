66baa6ce8cf3a9a7592452db39df939da259954e
mettled <smettled@gmail.com>
2020-03-04 20:36:17 -0800

    rename amountSpaces to indent

--- src/formatters/format-object.js
+++ src/formatters/format-object.js
@@ -1,4 +1,4 @@
-const amountSpaces = (count) => ' '.repeat(count);
+const indent = (count) => ' '.repeat(count);
 const PADDING = 4;
 
 const typeField = [
@@ -8,7 +8,7 @@ const typeField = [
   },
   {
     check: (arg) => arg instanceof Object,
-    getValue: (value, level) => `{\n${Object.entries(value).map(([key, field]) => (`${amountSpaces(level + PADDING)}${key}: ${field}`))}\n${amountSpaces(level)}}`,
+    getValue: (value, level) => `{\n${Object.entries(value).map(([key, field]) => (`${indent(level + PADDING)}${key}: ${field}`))}\n${indent(level)}}`,
   },
 ];
 
@@ -18,19 +18,19 @@ const checkItem = (value, level, f) => (
 
 const config = {
   removed: (item, level, f) => (
-    `${amountSpaces(level)}  - ${item.key}: ${checkItem(item.value, level, f)}`
+    `${indent(level)}  - ${item.key}: ${checkItem(item.value, level, f)}`
   ),
   added: (item, level, f) => (
-    `${amountSpaces(level)}  + ${item.key}: ${checkItem(item.value, level, f)}`
+    `${indent(level)}  + ${item.key}: ${checkItem(item.value, level, f)}`
   ),
   parent: (item, level, f) => (
-    `${amountSpaces(level)}    ${item.key}: {\n${f(item.children, level + PADDING)}\n${amountSpaces(level + PADDING)}}`
+    `${indent(level)}    ${item.key}: {\n${f(item.children, level + PADDING)}\n${indent(level + PADDING)}}`
   ),
   equal: (item, level) => (
-    `${amountSpaces(level)}    ${item.key}: ${item.value}`
+    `${indent(level)}    ${item.key}: ${item.value}`
   ),
   changed: (item, level, f) => (
-    `${amountSpaces(level)}  - ${item.key}: ${checkItem(item.valueBefore, level, f)}\n${amountSpaces(level)}  + ${item.key}: ${checkItem(item.valueAfter, level, f)}`
+    `${indent(level)}  - ${item.key}: ${checkItem(item.valueBefore, level, f)}\n${indent(level)}  + ${item.key}: ${checkItem(item.valueAfter, level, f)}`
   ),
 };
 
